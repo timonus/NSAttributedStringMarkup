18d3a0200cc875511123fdb3e1b7a0dee81fa706
Tim Johnsen <timonus@gmail.com>
2019-03-12 22:09:40 -0700

    Add source files.

---
+++ NSAttributedString+TJFormatting.h
@@ -0,0 +1,33 @@
+//
+//  NSAttributedString+TJFormatting.h
+//  OpenerCore
+//
+//  Created by Tim Johnsen on 2/10/18.
+//  Copyright © 2018 tijo. All rights reserved.
+//
+
+@import Foundation;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSAttributedString (TJFormatting)
+
+/**
+ Used for creating @c NSAttributedStrings using lightweight HTML-like formatting.
+ @param markupString An NSString containing HTML-like markup. For example "<b>Hello</b> world, how are <i>you</i> today!"
+ @param attributes An dictionary of default NSAttributedString attributes that are applied to the whole string.
+ @param customizerBlock A block that's called for each markup tag that's detected. Returns a dictionary of NSAttributedString attributes to apply to the span of text within the tag.
+ @note This method supports nested tags by default, you can use the method below with @c supportNesting set to @c NO as an optional perf optimization if your input doesn't contain nested tags.
+ */
++ (instancetype)attributedStringWithMarkupString:(NSString *const)markupString
+                                      attributes:(nullable NSDictionary *const)attributes
+                                 customizerBlock:(NSDictionary *(^)(NSString *tag))block;
+
++ (instancetype)attributedStringWithMarkupString:(NSString *const)markupString
+                                  supportNesting:(const BOOL)supportNesting
+                                      attributes:(nullable NSDictionary *const)attributes
+                                 customizerBlock:(NSDictionary *(^)(NSString *tag))block;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSAttributedString+TJFormatting.m
@@ -0,0 +1,65 @@
+//
+//  NSAttributedString+TJFormatting.m
+//  OpenerCore
+//
+//  Created by Tim Johnsen on 2/10/18.
+//  Copyright © 2018 tijo. All rights reserved.
+//
+
+#import "NSAttributedString+TJFormatting.h"
+
+@implementation NSAttributedString (TJFormatting)
+
++ (instancetype)attributedStringWithMarkupString:(NSString *const)markupString
+                                      attributes:(NSDictionary *const)attributes
+                                 customizerBlock:(NSDictionary *(^)(NSString *tag))block
+{
+    return [self attributedStringWithMarkupString:markupString
+                                   supportNesting:YES
+                                       attributes:attributes
+                                  customizerBlock:block];
+}
+
++ (instancetype)attributedStringWithMarkupString:(NSString *const)markupString
+                                  supportNesting:(const BOOL)supportNesting
+                                      attributes:(NSDictionary *const)attributes
+                                 customizerBlock:(NSDictionary *(^)(NSString *tag))block
+{
+    NSSet<NSString *> *tags = nil;
+    if (supportNesting) {
+        static NSRegularExpression *tagRegex;
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            tagRegex = [NSRegularExpression regularExpressionWithPattern:@"</?(.*?)>" options:0 error:nil];
+        });
+        
+        NSMutableSet<NSString *> *const mutableTags = [NSMutableSet new];
+        for (NSTextCheckingResult *const result in [tagRegex matchesInString:markupString options:0 range:NSMakeRange(0, markupString.length)]) {
+            [mutableTags addObject:[markupString substringWithRange:[result rangeAtIndex:1]]];
+        }
+        tags = mutableTags;
+    } else {
+        tags = [NSSet setWithObject:@".*?"];
+    }
+    
+    NSMutableAttributedString *const mutableAttributedString = [[NSMutableAttributedString alloc] initWithString:markupString attributes:attributes];
+    for (NSString *const tag in tags) {
+        NSString *const escapedTag = supportNesting ? [NSRegularExpression escapedPatternForString:tag] : tag;
+        NSRegularExpression *const regex = [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@"<(%1$@)>(.*?)</(%1$@)>", escapedTag] options:0 error:nil];
+        NSAssert(regex, @"Unable to form regex");
+        NSString *const underlyingString = mutableAttributedString.string;
+        for (NSTextCheckingResult *const result in [[regex matchesInString:underlyingString options:0 range:NSMakeRange(0, underlyingString.length)] reverseObjectEnumerator]) {
+            NSMutableDictionary *const mutableAttributes = [NSMutableDictionary dictionaryWithDictionary:attributes];
+            NSString *const parsedTag = supportNesting ? tag : [underlyingString substringWithRange:[result rangeAtIndex:1]];
+            NSAssert([[underlyingString substringWithRange:[result rangeAtIndex:3]] isEqualToString:parsedTag], @"Mismatching tags! %@ - %@", parsedTag, [underlyingString substringWithRange:[result rangeAtIndex:3]]);
+            [mutableAttributes addEntriesFromDictionary:block(parsedTag)];
+            NSString *const text = [underlyingString substringWithRange:[result rangeAtIndex:2]];
+            [mutableAttributedString replaceCharactersInRange:result.range
+                                         withAttributedString:[[NSAttributedString alloc] initWithString:text attributes:mutableAttributes]];
+        }
+    }
+    
+    return mutableAttributedString;
+}
+
+@end
