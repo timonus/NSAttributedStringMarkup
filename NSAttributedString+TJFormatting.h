e76d1a62686aa17b185258f2a576cde54a2d17d9
Tim Johnsen <timonus@gmail.com>
2023-01-18 20:57:52 -0800

    Fix nullability annotation.

--- NSAttributedString+TJFormatting.h
+++ NSAttributedString+TJFormatting.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef NSDictionary<NSAttributedStringKey, id> *_Nullable (^TJFormattingCustomizerBlock)(NSString *const tag, NSDictionary<NSAttributedStringKey, id> *attributes);
+typedef NSDictionary<NSAttributedStringKey, id> *_Nullable (^TJFormattingCustomizerBlock)(NSString *const tag, NSDictionary<NSAttributedStringKey, id> *_Nullable attributes);
 @interface NSAttributedString (TJFormatting)
 
 /**
